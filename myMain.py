c8c1bed0bd979566001254b3e4ba6a12b58f7f07
hysspoluo <517645428@qq.com>
2019-04-08 17:32:12 +0800

    Initial commit

--- myMain.py
+++ myMain.py
@@ -26,12 +26,13 @@ def htmlwriteinTxt(pageSource,txtName):
     fp = open(txtName, "w",encoding='utf-8')
     fp.write(pageSource)
     fp.close()
+#分析获取的网页信息
 def analysishtml(txtName):
     soup = BeautifulSoup(open(txtName,"r",encoding='utf-8'),'html.parser')
     table = soup.find("table",id="scheTab")
     trs = table.find_all("tr")
     trs_len = len(trs)
-    result_list = []
+    Schedule_List = []
     first_link = "http://nba.win007.com"
     for tr_num in range(1, trs_len):
         tds = trs[tr_num].find_all("td")
@@ -52,16 +53,18 @@ def analysishtml(txtName):
             result["analysis_link"] = first_link + (tds[7].a)['href']
         elif tds[7].text == "[欧]":
             result["euro_link"] = first_link + (tds[7].a.next_sibling)['href']
-        result_list.append(result)
-    for one in result_list:
-        print(one)
+        Schedule_List.append(result)
 
+    return Schedule_List
+
+
+#创建球队的表，每个表有球队这个赛季的所有赛程
 def createTables(teamlist):
     conn = sqlite3.connect('nba.db')
     print("连接数据库，没有则新建")
     cursor = conn.cursor()
     for team in teamlist:
-        createtableString = 'create table if not exists "%s" (id int primary key,type text,' \
+        createtableString = 'create table if not exists "%s" (season text,type text,' \
                             'time text,home text,result text,guest text,handicap text,' \
                             'totle text,analysis_Link text,euro_Link text)'%(team)
         cursor.execute(createtableString)
@@ -69,11 +72,37 @@ def createTables(teamlist):
     conn.commit()
     conn.close()
 
+def writeIntoDatabase(Schedule_List,teamName):
+    conn = sqlite3.connect('nba.db')
+    print("打开数据库，对表进行读写")
+    cursor = conn.cursor()
+
+    for game in Schedule_List:
+        season = "2018-2019"
+
+        insetString = 'insert into %s (season,type,time,home,result,guest,handicap,totle,analysis_link,euro_link)' \
+                     'values("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s")'\
+        %(teamName,season,game['type'],game['time'],game['home'],game['result'],game['guest'],game['handicap'],game['totle'],game['analysis_link'],game['euro_link'])
+        print(insetString)
+        '''
+        insertString = 'insert into %s (id,season,type,time,home,result,guest,handicap,totle,analysis_Link,euro_Link)' \
+                       'values (%d,"2009-2010","常规赛","十点","","","","","","","")'%(teamName,id)
+        game['type'],game['time'],game['home'],game['result'],game['guest'],game['handicap'],game['totle'],game['analysis_link'],game['euro_link']
+        '''
+        cursor.execute(insetString)
+        conn.commit()
+
+
+    #conn.commit()
+    conn.close()
+
 
 
 if __name__=='__main__':
     #开始程序
+
     createTables(teamList)
+    '''
     myDriver = webdriver.Chrome()
     #time.sleep(5)
     myDriver.get(seasonLink)
@@ -86,6 +115,7 @@ if __name__=='__main__':
 
     teamOptions = teamSelect.options
     optionNum = 0
+    #开始写入文件html
     for team in teamOptions:
         if team.text in teamList:
             teamSelect.select_by_index(optionNum)
@@ -101,5 +131,15 @@ if __name__=='__main__':
             htmlwriteinTxt(teamHtml,txtName)
             #time.sleep(3)
         optionNum += 1
-    analysishtml("勇士.html")
-    myDriver.quit()
+    #分析文件,写入数据库
+    analysishtml("灰熊.html")
+    '''
+
+    #for team in teamList:
+     #   teamNameFile = team+".html"
+     #   Schedule_List = analysishtml(teamNameFile)
+    #    writeIntoDatabase(Schedule_List,team)
+    #myDriver.quit()
+    Schedule_List = analysishtml("灰熊.html")
+    writeIntoDatabase(Schedule_List, "灰熊")
+
